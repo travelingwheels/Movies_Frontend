b3380029f16df6a31a647286fe068bae936d7fdf
Gary <gkwheeler40@gmail.com>
2020-10-05 20:25:00 -0600

    build out fetch(get request) function for index

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello from index.js");
+const endpoint = ("http://localhost:3000/api/v1/movies")
+
+document.addEventListener('DOMContentLoaded', () => {
+    getMovie()
+})
+
+function getMovie() {
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(movies => {
+        movies.data.forEach(movie => {
+            const movieMarkup = `
+              <div data-id=${movie.id}>
+                <img src=${movie.attributes.image_url} height="200" width="250">
+                <h3>${movie.attributes.title}</h3>
+                <p>${movie.attributes.description}</p>
+                <p>${movie.attributes.category.name}</p>
+                <button data-id=${movie.id}>edit</button>
+              </div>
+              <br><br>`;
+    
+              document.querySelector('#movie-container').innerHTML += movieMarkup
+            })
+        })
+    }
