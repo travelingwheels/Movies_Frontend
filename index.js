b3746c2d1a5b27ae2b7b8fae08b3685e27e0d6ac
Gary <gkwheeler40@gmail.com>
2020-10-16 21:35:54 -0600

    add fetch post

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-const endpoint = ("http://localhost:3000/api/v1/movies")
+const endpoint = "http://localhost:3000/api/v1/movies"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getMovie()
+    getFetch()
 
-    const newFormData = document.querySelector("#create-movie-form")
-    newFormData.addEventListener("submit", (e) => dataHandler(e))
+    const newFormData = document.querySelector("#new-movie-form")
+    newFormData.addEventListener("submit", (e) => submitHandler(e))
 })
 
-function getMovie() {
+function getFetch() {
     fetch(endpoint)
     .then(res => res.json())
     .then(movies => {
         movies.data.forEach(movie => {
-            const movieMarkup = `
+            const renderMovie = `
               <div data-id=${movie.id}>
-                <img src=${movie.attributes.image_url} height="200" width="250">
+                <img src=${movie.attributes.image_url} height="300" width="250">
                 <h3>${movie.attributes.title}</h3>
                 <p>${movie.attributes.description}</p>
                 <p>${movie.attributes.category.name}</p>
-                <button data-id=${movie.id}>edit</button>
+                
               </div>
               <br><br>`;
     
-              document.querySelector('#movie-container').innerHTML += movieMarkup
-            })
+            document.querySelector('#movie-container').innerHTML += renderMovie
         })
-    }
+    })
+}
 
-    function dataHandler(e) {
-        e.preventDefault()
-        const titleInput = document.querySelector('#input-title').value
-        const descriptionInput = document.querySelector('#input-description').value
-        const imageInput = document.querySelector('#input-url').value
-        const categoryInput = document.querySelector('#categories').value
-    }
+    
+
+function submitHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, descriptionInput, imageInput, categoryId)
+}
+
+function postFetch(title, description, image_url, category_id) {
+     //console.log(title, description, image_url, category_id)
+    fetch(endpoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json",
+                     "Accept": "application/json"},
+         body: JSON.stringify({
+            title: title,
+            description: description,
+            image_url: image_url,
+            category_id: category_id
+        })
+    })
+        .then(res => res.json())
+        .then(movie => {
+        //console.log(movie);
+        const renderMovie = `
+              <div data-id=${movie.id}>
+                <img src=${movie.data.attributes.image_url} height="300" width="250">
+                <h3>${movie.data.attributes.title}</h3>
+                <p>${movie.data.attributes.description}</p>
+                <p>${movie.data.attributes.category.name}</p>
+                
+              </div>
+              <br><br>`;
+        
+        document.querySelector('#movie-container').innerHTML += renderMovie;
+    })
+}
