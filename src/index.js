3c000becbd3308737316d74684eeb4ff151f7d12
Gary <gkwheeler40@gmail.com>
2020-11-24 23:32:04 -0700

    add to subitHandler to pass star data to the backend

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     getFetch()
 
     const newFormData = document.querySelector("#movie-form")
-    newFormData.addEventListener("submit", (e) => submitHandler(e))
+    newFormData.addEventListener("submit", (e) => submitHandler(e)) 
 
     const searchBar = document.getElementById('searchBar')
     searchBar.addEventListener("keyup", (e) => keyUpHandler(e))
@@ -30,11 +30,13 @@ function getFetch() {
      .catch(error => console.log(error));
 }
 
+
+
     
 function keyUpHandler(e) {
     const searchString = e.target.value.toLowerCase();
     const filteredMovie = frontendMovies.filter((selectMovie) => {
-        return (selectMovie.title.toLowerCase().includes(searchString));
+        return selectMovie.title.toLowerCase().includes(searchString);
     });
     let filteredArray = []
     filteredMovie.forEach(movie => {
@@ -52,17 +54,27 @@ function keyUpHandler(e) {
 
 function submitHandler(e) {
     e.preventDefault()
+    
+        let rate
+        let star = document.querySelector('input:checked')
+        if(!star){
+            rate = 0
+        }
+        else {
+            rate = star.id.slice(-1)
+        }
     const titleInput = document.querySelector('#input-title').value
     const descriptionInput = document.querySelector('#input-description').value
     const imageInput = document.querySelector('#input-url').value
+    const ratingInput = rate
     const categoryId = parseInt(document.querySelector('#categories').value)
-    postFetch(titleInput, descriptionInput, imageInput, categoryId)
-}
+    postFetch(titleInput, descriptionInput, imageInput, ratingInput, categoryId)
+};
 
 
 
 
-function postFetch(title, description, image_url, category_id) {
+function postFetch(title, description, image_url, rating,  category_id) {
     fetch(endPoint, {
         method: "POST",
         headers: {"Content-Type": "application/json",
@@ -72,19 +84,20 @@ function postFetch(title, description, image_url, category_id) {
             title: title,
             description: description,
             image_url: image_url,
+            rating: rating,
             category_id: category_id
         })
     })
-        .then(res => res.json())
-        .then(movie => {
-            const newMovie = movie.data
-            let movieData = new Movie(newMovie, newMovie.attributes)
-            frontendMovies.push(movieData)
-            const sorted = Movie.sortByTitle(frontendMovies);
-            document.querySelector('#movie-container').innerHTML = " "
-            sorted.forEach(movie => {
-            document.querySelector('#movie-container').innerHTML += movie.renderMovie()   
-            })  
-         })
-         .catch(error => console.log(error));
+    .then(res => res.json())
+    .then(movie => {
+        const newMovie = movie.data
+        let movieData = new Movie(newMovie, newMovie.attributes)
+        frontendMovies.push(movieData)
+        const sorted = Movie.sortByTitle(frontendMovies);
+        document.querySelector('#movie-container').innerHTML = " "
+        sorted.forEach(movie => {
+            document.querySelector('#movie-container').innerHTML += movie.renderMovie() 
+        }) 
+    })
+        .catch(error => console.log(error));
 }
